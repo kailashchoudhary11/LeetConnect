79ec6ac79fd059b12e7849d3e4bc779b8cca1c1e
Kailash Choudhary <kailashjat604@gmail.com>
2023-10-04 20:20:34 +0530

    feat: migrate database to postgres

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -74,10 +74,14 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+   'default': {
+       'ENGINE': 'django.db.backends.postgresql',
+       'NAME': os.getenv('POSTGRES_DB'),
+       'USER': os.getenv('POSTGRES_USER'),
+       'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
+       'HOST': 'db',
+       'PORT': 5432,
+   }
 }
 
 
