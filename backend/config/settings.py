e525d4935eb0959b9152900c730a578c2eaf1b41
Kailash Choudhary <kailashjat604@gmail.com>
2023-10-09 20:55:12 +0530

    fix: static root path

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -137,7 +137,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = "/staticfiles/"
+
+if not DEBUG:
+    STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
--- backend/start.sh
+++ backend/start.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
 python manage.py migrate
+
+if [ "$DEBUG" != "1" ]; then
+  python manage.py collectstatic --no-input
+fi
+
 python manage.py runserver 0.0.0.0:8000
