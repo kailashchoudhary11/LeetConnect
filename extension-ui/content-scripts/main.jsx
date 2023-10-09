d4c49d64dc75f77adf6e6ba944feb9614f59bb0f
Kailash Choudhary <kailashjat604@gmail.com>
2023-10-10 01:51:32 +0530

    feat: fetch solution from backend

--- extension-ui/content-scripts/main.jsx
+++ extension-ui/content-scripts/main.jsx
@@ -1,16 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import "./index.css";
 
-const navbar = document.getElementsByClassName("text-label-2 dark:text-dark-label-2 flex w-full items-center")[0];
 const root = document.createElement("div");
-root.style.margin = "0";
-root.style.padding = "0";
-root.classList.add("cursor-pointer");
-root.id = "lc-root";
-const thirdChild = navbar.children[2];
-navbar.insertBefore(root, thirdChild.nextSibling);
+root.id = "crx-root";
+const node = document.getElementsByClassName("text-label-2 dark:text-dark-label-2 flex w-full items-center")[0]
+console.log(node);
+node.appendChild(root);
+// document.body.appendChild(root);
 
 ReactDOM.createRoot(root).render(
   <React.StrictMode>
