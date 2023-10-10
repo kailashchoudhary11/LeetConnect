3af9003c309cb68dfd24ae9b4ebb751562097af0
Kailash Choudhary <kailashjat604@gmail.com>
2023-10-10 16:37:23 +0530

    feat: display first matched solution after search

--- extension-ui/content-scripts/SearchBox.jsx
+++ extension-ui/content-scripts/SearchBox.jsx
@@ -1,13 +1,26 @@
 import { useState } from "react";
 import "./SearchBox.css";
 import axios from "axios";
+import { getSolutionHTML, getSlug } from "./utils";
 
 export default function SearchBox() {
   const [searchText, setSearchText] = useState("");
   async function searchSolution() {
+    if (searchText == "") return;
+
     const userName = document.getElementsByClassName("text-label-3 dark:text-dark-label-3 text-xs")[0].textContent;
-    const res = await axios.post("https://leet-connect.onrender.com/api/solution/", {userName: userName, questionSlug:searchText});
-    console.log("Result is", res.data);
+    const questionSlug = getSlug(searchText);
+
+    setSearchText("");
+
+    const res = await axios.post("https://leet-connect.onrender.com/api/solution/", { username: userName, "question-slug": questionSlug });
+    const data = res.data;
+
+    const solution = data.questionSolutions.solutions[0];
+
+    const container = document.getElementsByClassName("text-label-2 dark:text-dark-label-2 flex w-full items-center")[0].nextElementSibling;
+    
+    container.innerHTML = getSolutionHTML(searchText, solution);
   }
   return (
     <div className="search-box-container">
