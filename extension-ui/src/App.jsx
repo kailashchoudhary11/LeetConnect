604d549588216a2fa04538a0efdf94eda8f3628a
Hemant Chaudhary <104961126+hemant933@users.noreply.github.com>
2023-12-24 18:10:53 +0530

    Update App.jsx
    Implement error handling for asynchronous operations

--- extension-ui/src/App.jsx
+++ extension-ui/src/App.jsx
@@ -6,51 +6,62 @@ import FollowingList from "./components/FollowingList";
 import axios from "axios";
 import BASE_URL from "./utils/BASE_URL";
 
-function App() {
+async function fetchUserData(urlObj) {
+   const path = urlObj.pathname;
+   const regex = /^\/([^/]+)\/$/;
+   const match = path.match(regex);
 
-  // const exclude = ["explore", "contest", "discuss", "interview", "assessment", "store", "subscribe"]
-
-  const [following, setFollowing] = useState([]);
-  const [user, setUser] = useState({});
-
-  useEffect(() => {
-    async function updateFollowing() {
-      const existingFollowing = await chrome.storage.local.get(["following"])
-      const data = JSON.parse(existingFollowing.following);
-      if (data) {
-        setFollowing(data);
+   if (match) {
+      const uName = match[1];
+      try {
+         const res = await axios.get(`${BASE_URL}/api/users/${uName}`);
+         if (!("error" in res.data)) {
+            return res.data.matchedUser;
+         }
+      } catch (error) {
+         console.error("Error fetching user data:", error);
       }
-    }
-    updateFollowing();
-
-    chrome.tabs.query({ active: true, currentWindow: true }, async function (tabs) {
-      const curTabUrl = await tabs[0].url;
-      const urlObj = new URL(curTabUrl);
-      const host = urlObj.hostname;
-
-      if (host == "leetcode.com") {
-        const path = urlObj.pathname;
-        const regex = /^\/([^/]+)\/$/;
-        const match = path.match(regex);
-
-        if (match) {
-          const uName = match[1];
-          const res = await axios.get(`${BASE_URL}/api/users/${uName}`);
-          if (!("error" in res.data)) {
-            setUser(res.data.matchedUser);
-          }
-        }
-      }
-    });
-
-  }, []);
-
-  return (
-    <div>
-      <h1 className="text-3xl font-bold underline text-center">LeetConnect</h1>
-      {user?.username && <FollowButton user={user} following={following} setFollowing={setFollowing} />}
-      <FollowingList following={following} setFollowing={setFollowing} />
-    </div>);
+   }
+   return null;
 }
 
-export default App
+function App() {
+   const [following, setFollowing] = useState([]);
+   const [user, setUser] = useState({});
+
+   useEffect(() => {
+      async function fetchData() {
+         const existingFollowing = await chrome.storage.local.get(["following"]);
+         const data = JSON.parse(existingFollowing.following);
+         if (data) {
+            setFollowing(data);
+         }
+
+         const tabs = await chrome.tabs.query({ active: true, currentWindow: true });
+         const curTabUrl = tabs[0]?.url;
+         if (curTabUrl) {
+            const urlObj = new URL(curTabUrl);
+            const host = urlObj.hostname;
+
+            if (host === "leetcode.com") {
+               const userData = await fetchUserData(urlObj);
+               if (userData) {
+                  setUser(userData);
+               }
+            }
+         }
+      }
+
+      fetchData();
+   }, []);
+
+   return (
+      <div>
+         <h1 className="text-3xl font-bold underline text-center">LeetConnect</h1>
+         {user?.username && <FollowButton user={user} following={following} setFollowing={setFollowing} />}
+         <FollowingList following={following} setFollowing={setFollowing} />
+      </div>
+   );
+}
+
+export default App;
