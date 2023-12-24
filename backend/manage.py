6b2416ad2e592fbc12c438cde8e1bf1d1e37cf7b
Hemant Chaudhary <104961126+hemant933@users.noreply.github.com>
2023-12-24 18:13:20 +0530

    Update manage.py
    remove redundant comments

--- backend/manage.py
+++ backend/manage.py
@@ -12,10 +12,10 @@ def main():
     except ImportError as exc:
         raise ImportError(
             "Couldn't import Django. Are you sure it's installed and "
-            "available on your PYTHONPATH environment variable? Did you "
-            "forget to activate a virtual environment?"
+            "available on your PYTHONPATH? Did you forget to activate a virtual environment?"
         ) from exc
     execute_from_command_line(sys.argv)
+
 
 
 if __name__ == '__main__':
